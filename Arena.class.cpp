52eb96775956a350159c8e5d6f014c15384e134d
Rodger FRANCIS <rfrancis@e4r16p5.wethinkcode.co.za>
2018-06-09 14:16:12 +0200

    Arena.class.cpp

--- Arena.class.cpp
+++ Arena.class.cpp
@@ -3,7 +3,22 @@
 
 Arena :: Arena()
 {
+    this->x = 10;
+    this->y = 50;
+    this->area = new GameEntity*[x];
+    Player p;
 
+    Empty placeholder;
+    
+    for (int i = 0; i < this->y; ++i)
+        this->area[i] =new GameEntity[y];
+   
+    for(int j = 0; j < this->x; ++j){
+        for(int k = 0; k < this->y; ++k){            
+            this->area[j][k] = placeholder;   
+        }
+    }
+    //this->area[(this->x - 1) ][(this->y -1) /2] = p;
 }
 
 
@@ -15,16 +30,22 @@ Arena :: Arena (int x, int y)
 {
     this->x = x;
     this->y = y;
-    this->area = new short*[x];
-    for (int i = 0; i < y; i++)
-        this->area[i] =new short[y];
+    this->area = new GameEntity*[x];
+    Empty placeholder;
+    Player p;
+    Enemy e;
 
-    for(int j = 0; j < this->x; ++j){
-        for(int k = 0; k < this->y; ++k){            
-            this->area[j][k] = 0;
-            std::cout << this->area[j][k];
+    for (int i = 0; i < this->y; i++)
+        this->area[i] = new GameEntity[y];
+
+     for(int j = 0; j < this->x; j++){
+        for(int k = 0; k < this->y; k++){            
+            this->area[j][k] = placeholder; 
         }
     }
+   this->area[(this->x - 1) ][(this->y -1) /2] = p;
+    for (int i = 0; i < this->x; i++)
+        this->area[0][i] = e;
 }
 
 Arena :: ~Arena ()
@@ -41,4 +62,45 @@ short Arena :: getX(){
 
 short Arena :: getY(){
  return this->y;
+}
+
+
+void Arena :: eval(){
+    Empty placeholder;
+
+    for(int j = 0; j < this->x; ++j){
+        std::cout << std::endl;
+        for(int k = 0; k < this->y; ++k){            
+            std::cout << this->area[j][k].getSig() << ' ';
+           switch (this->area[j][k].getSig())
+            {
+                case 0:
+                    break;
+                case 1:
+                    break;
+                case 2:
+                    if (this->area[j][k].getDirection() == 'u')
+                    {
+                        if (this->area[j][k - 1].isAlive())
+                        {
+                            this->area[j][k - 1] = placeholder;                            
+                        }
+                        this->area[j][k] = placeholder;
+
+                    }
+                    if (this->area[j][k].getDirection() == 'd')
+                    {
+                         if (this->area[j][k + 1].isAlive())
+                        {
+                            this->area[j][k+1] = placeholder;                            
+                        }
+                        this->area[j][k] = placeholder;
+                    }
+                    break;
+                default:
+                    break;
+
+            }
+        }
+    }
 }
