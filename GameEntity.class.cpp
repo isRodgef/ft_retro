738012f1d68c76b812478d00967a998efb9d0078
Rodger FRANCIS <rfrancis@e4r16p5.wethinkcode.co.za>
2018-06-09 14:16:19 +0200

    GameEntity.class.cpp

--- GameEntity.class.cpp
+++ GameEntity.class.cpp
@@ -1,16 +1,109 @@
 #include "GameEntity.hpp"
 
+
 GameEntity :: GameEntity()
 {
 
 }
 
-GameEntity :: GameEntity(bool player)
-{
-    this->player = player;
-}
-
 GameEntity :: ~GameEntity()
 {
 
+}
+
+bool GameEntity :: isplayable() const
+{
+    return this->player;
+}
+
+char GameEntity :: getDirection()
+{
+    return 'a';
+}
+
+void GameEntity :: operator =(const GameEntity &g)
+{
+    if (this != &g) 
+    {
+        this->alive = g.isAlive();
+        this->player = g.isplayable();
+        this->signature = g.getSig();
+    }
+}
+
+void GameEntity :: die()
+{
+    this->alive = false;
+}
+
+short GameEntity :: getSig() const
+{
+    return this->signature;
+}
+
+bool GameEntity :: isAlive() const
+{
+    return this->alive;
+}
+
+Player :: Player()
+{
+    this->player = true;
+    this->score = 0;
+    this->alive = true;
+    this->signature = 1;
+}
+
+Player :: ~Player()
+{
+
+}
+
+Enemy :: Enemy()
+{
+    this->player = false;
+    this->alive = true;
+    this->signature = -1;
+}
+
+Enemy :: ~Enemy()
+{
+
+}
+
+Empty :: Empty()
+{
+    this->player = false;
+    this->alive = false;
+    this->signature = 0;
+}
+
+Empty :: ~Empty()
+{
+  
+}
+
+Bullet :: Bullet()
+{
+    this->player = false;
+    this->alive = true;
+    this->signature = 2;
+}
+
+Bullet :: ~Bullet()
+{
+
+}
+
+Bullet :: Bullet(char dir)
+{
+    this->player = false;
+    this->alive = true;
+    this->signature = 2;
+    this->direction = dir;
+}
+
+char Bullet :: getDirection()
+{
+    return this->direction;
 }
