444c3a14cdf0e394868adb8535b53bcac5978272
Rodger FRANCIS <rfrancis@e4r16p5.wethinkcode.co.za>
2018-06-09 16:11:26 +0200

    Arena.hpp

--- Arena.hpp
+++ Arena.hpp
@@ -9,13 +9,17 @@ class Arena
     int x;
     int y;
     GameEntity **area; 
-    
+    bool  even;
+
     public:
         Arena();
         Arena(int x, int y);
         short getX();
         short getY();
         void eval();
+        GameEntity **get_area();
+        void moveLeft();
+        void moveRight();
         ~Arena();
 };
 
