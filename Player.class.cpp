fd017757af5ecd51c48ddde70c2e33a18e34beb1
Rodger FRANCIS <rfrancis@e4r16p5.wethinkcode.co.za>
2018-06-10 18:12:10 +0200

    Player.class.cpp

---
+++ Player.class.cpp
@@ -0,0 +1,15 @@
+
+#include "Player.hpp"
+
+Player :: Player()
+{
+    this->player = true;
+    this->score = 0;
+    this->alive = true;
+    this->signature = 1;
+}
+
+Player :: ~Player()
+{
+
+}
